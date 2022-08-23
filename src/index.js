5f6e7430e023857652e3ab4dd7ab19c1e505c195
Shending-Help <yusufcheetos99@gmail.com>
2022-08-23 04:19:08 +0200

    tweets a random joke

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import Twitter from "twitter";
-import { getTweets } from "./tweets";
+import client from "./twitterClient";
 import getJoke from "./joker";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
+const tweet = async () => {
+  const joke = await getJoke();
+  const tweet = `${joke.title}\n${joke.body}`;
+  await client.post("statuses/update", { status: tweet });
+};
---
+++ src/twitterClient.js
@@ -0,0 +1,13 @@
+import Twitter from "twitter";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+export default client;
